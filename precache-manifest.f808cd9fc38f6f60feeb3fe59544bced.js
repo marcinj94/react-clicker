126a20029445402858a12da8837da249cdb57d8f
beardofdoom <marcin.jusiak@gmail.com>
2019-01-25 21:57:28 +0100

    Updates

---
+++ precache-manifest.f808cd9fc38f6f60feeb3fe59544bced.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "769b30449b9388969b36",
+    "url": "/react-clicker/static/js/runtime~main.769b3044.js"
+  },
+  {
+    "revision": "78fc8b48a6e965231b7f",
+    "url": "/react-clicker/static/js/main.78fc8b48.chunk.js"
+  },
+  {
+    "revision": "6f90670de942cd129c87",
+    "url": "/react-clicker/static/js/1.6f90670d.chunk.js"
+  },
+  {
+    "revision": "78fc8b48a6e965231b7f",
+    "url": "/react-clicker/static/css/main.a49fd717.chunk.css"
+  },
+  {
+    "revision": "3c65fb759b3bce1f0624ea0381b56aa2",
+    "url": "/react-clicker/index.html"
+  }
+];
